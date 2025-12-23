9578a19f6d631ec5d64dc6cb30c65beb1c271623
SwayZay <leftumbrella@outlook.com>
2025-12-23 08:59:38 +0800

    增加对框的绘制

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <onnxruntime/onnxruntime_cxx_api.h>
 #include <vector>
 #include <iostream>
-
+struct Border {
+    float fPossibility;
+    unsigned short usX1;
+    unsigned short usY1;
+    unsigned short usX2;
+    unsigned short usY2;
+};
 int main() {
     cv::Mat img = cv::imread("images/times.jpg");
     if (img.empty()) {
@@ -74,27 +80,42 @@ int main() {
     float* out = output_tensors[0].GetTensorMutableData<float>();
     float fThreshold = 0.5;
 
-    std::map<unsigned int, std::pair<unsigned short,float>> mapBeastType;
+    std::map<unsigned int, std::pair<unsigned short,Border>> mapBeastType;
     for (auto i=0;i<8400;++i) {
         // 找出80类中，本检测单元概率最大的类别
         std::pair<int,float> pairBestType(0,-1);
+        std::array<float,4> arrXYWH{};
         for (auto j=0;j<80;++j) {
             float fThis = out[(4 + j) * 8400 + i];
             float fPossibility = fThis;//1.0f / (1.0f + std::exp(-fThis));
             if (fPossibility>pairBestType.second) {
                 pairBestType.second = fPossibility;
                 pairBestType.first = j;
+                arrXYWH[0] = out[0 * 8400 + i];
+                arrXYWH[1] = out[1 * 8400 + i];
+                arrXYWH[2] = out[2 * 8400 + i];
+                arrXYWH[3] = out[3 * 8400 + i];
             }
         }
         if (pairBestType.second > 0 && pairBestType.second>fThreshold) {
+            auto usX1 = static_cast<unsigned short>(arrXYWH[0] - (arrXYWH[2] / 2.0));
+            auto usY1 = static_cast<unsigned short>(arrXYWH[1] - (arrXYWH[3] / 2.0));
+            auto usX2 = static_cast<unsigned short>(arrXYWH[0] + (arrXYWH[2] / 2.0));
+            auto usY2 = static_cast<unsigned short>(arrXYWH[1] + (arrXYWH[3] / 2.0));
             mapBeastType[i].first = pairBestType.first;
-            mapBeastType[i].second = pairBestType.second;
+            mapBeastType[i].second.fPossibility = pairBestType.second;
+            mapBeastType[i].second.usX1 = usX1;
+            mapBeastType[i].second.usY1 = usY1;
+            mapBeastType[i].second.usX2 = usX2;
+            mapBeastType[i].second.usY2 = usY2;
         }
     }
 
     for (auto& elem:mapBeastType) {
-        std::cout << elem.first << "\t" << elem.second.first<< "\t" << elem.second.second << std::endl;
+        cv::rectangle(img_resized,cv::Point(elem.second.second.usX1,elem.second.second.usY1),cv::Point(elem.second.second.usX2,elem.second.second.usY2), cv::Scalar(0, 255, 0),3);
+        std::cout << elem.first << "\t" << elem.second.first<< "\t" << elem.second.second.fPossibility << std::endl;
     }
-
+    cv::imshow("box", img_resized);
+    cv::waitKey(0);
     return 0;
 }
